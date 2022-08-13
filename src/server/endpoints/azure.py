fa847876474437c96303d13e1697c0020b933118
Paul Clarke <paulioclarke@icloud.com>
2022-08-13 17:56:28 +1000

    api scaffolding

---
+++ src/server/endpoints/azure.py
@@ -0,0 +1,12 @@
+# endpoints to do with connecting to azure and uploading files
+
+'''
+Uploads a Photo to Azure (POST)
+    Params:
+        - Unknown...
+        - plantId: int???
+    Returns:
+        - photoUrl: string??
+'''
+def upload_photo():
+    pass
---
+++ src/server/endpoints/users.py
@@ -0,0 +1,34 @@
+# User Information Management
+
+# ===== Major User Endpoints ====
+
+'''
+Adds a User (POST)
+    - Params:
+        - username:  string
+        - email:     string
+        - startDate: date
+'''
+def add_user():
+    pass
+
+'''
+Gets a User (GET)
+    - Params:
+        - userId: int
+    - Returns:
+        - all user info
+'''
+def get_user():
+    pass
+
+# === Minor User Endpoints ====
+
+'''
+Set User Bio
+    - Params:
+        - userId:   int
+        - bio:      string
+'''
+def set_user_bio():
+    pass
