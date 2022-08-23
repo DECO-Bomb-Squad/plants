4a6eecc31cc41792174a7f8940770e2f8ed5d95b
Satya Bhasale <95593545+venndiagram5@users.noreply.github.com>
2022-08-23 22:26:54 +1000

    Pot 4 (#6)
    * placeholder main screen
    
    * POT-4: tiny changes
    
    * POT-4: PR changes

--- src/app/lib/base/root_widget.dart
+++ src/app/lib/base/root_widget.dart
@@ -1,5 +1,7 @@
 import 'package:app/base/nav_bar.dart';
 import 'package:app/demo/demo_widget.dart';
+import 'package:app/screens/main_screen.dart';
+import 'package:app/utils/colour_scheme.dart';
 import 'package:flutter/material.dart';
 
 class RootWidget extends StatefulWidget {
@@ -9,8 +11,7 @@ class RootWidget extends StatefulWidget {
   State<RootWidget> createState() => _RootWidgetState();
 }
 
-class _RootWidgetState extends State<RootWidget>
-    with SingleTickerProviderStateMixin {
+class _RootWidgetState extends State<RootWidget> with SingleTickerProviderStateMixin {
   _RootWidgetState();
 
   @override
@@ -18,13 +19,23 @@ class _RootWidgetState extends State<RootWidget>
     super.initState();
     navItems = [
       NavBarItem(
-        text: "Demo",
-        iconData: Icons.question_answer,
-        childFunc: () => DemoWidget("Demo! Hello"),
+        text: "Home",
+        iconData: Icons.home,
+        childFunc: () => const MainScreen(),
       ),
       NavBarItem(
-        text: "Second Thing",
-        iconData: Icons.two_k,
+        text: "My Plants",
+        iconData: Icons.yard,
+        childFunc: () => const Text("boo"),
+      ),
+      NavBarItem(
+        text: "Trending",
+        iconData: Icons.trending_up,
+        childFunc: () => const Text("boo"),
+      ),
+      NavBarItem(
+        text: "Ask\nQuestion",
+        iconData: Icons.question_answer,
         childFunc: () => const Text("boo"),
       ),
     ];
@@ -37,15 +48,40 @@ class _RootWidgetState extends State<RootWidget>
 
   @override
   Widget build(BuildContext context) => Scaffold(
+        body: NestedScrollView(
+            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  backgroundColor: lightColour,
+                  shadowColor: lightColour,
+                  pinned: false,
+                  floating: true,
+                  forceElevated: innerBoxIsScrolled,
+                  iconTheme: const IconThemeData(color: darkHighlight, size: 35),
+                  actions: [
+                    IconButton(
+                      icon: const Icon(Icons.person),
+                      tooltip: 'Add new entry',
+                      onPressed: () {/* ... */},
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.notifications),
+                      tooltip: 'Add new entry',
+                      onPressed: () {/* ... */},
+                    ),
+                  ],
+                ),
+              ];
+            },
+            body: TabBarView(
+              controller: controller!,
+              children: navItems!.map((e) => e.child).toList(),
+            )),
         bottomNavigationBar: BottomAppBar(
           child: NavBar(
             controller: controller!,
             items: navItems!,
           ),
-        ),
-        body: TabBarView(
-          controller: controller!,
-          children: navItems!.map((e) => e.child).toList(),
         ),
       );
 }
