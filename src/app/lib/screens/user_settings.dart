b8b205a61f15feb5477ccf20fece8bfa82888fc8
Callum Dixon <cdixon4335@gmail.com>
2022-09-07 13:50:12 +1000

    added example buttons, can modify in future

--- src/app/lib/screens/user_settings.dart
+++ src/app/lib/screens/user_settings.dart
@@ -13,12 +13,39 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  // to add -> a back button?
+  bool isSelected = true;
+  List<DropdownMenuItem> exampleDropDown = <DropdownMenuItem>[]; // might need to add stuff here lol
+
   @override
   Widget build(BuildContext context) => Scaffold(
           body: NestedScrollView(
         headerSliverBuilder: StandardHeaderBuilder,
         body: Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                "SETTINGS",
+                style: mainHeaderStyle,
+              ),
+            ),
+            spacer,
+            Switch(value: isSelected, onChanged: ((value) => !isSelected)), // example switch
+            spacer,
+            DropdownButton(items: exampleDropDown, onChanged: ((value) => value = value)), // example drop down
+            spacer,
+            // add button
+            ElevatedButton(
+                onPressed: () => {
+                      // example button
+                      // idk button does something lmao add it here
+                    },
+                child: const Text("yes the button did something") // another thing to change
+                ),
+          ]),
         ),
       ));
+  SizedBox get spacer => const SizedBox(height: 10, width: 10);
 }
