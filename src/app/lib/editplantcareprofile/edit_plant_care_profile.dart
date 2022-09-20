556f9651016c99a8457f46fc634437f0f6bc0edb
Callum Dixon <cdixon4335@gmail.com>
2022-09-20 10:20:31 +1000

    added framework to build widget,

--- src/app/lib/editplantcareprofile/edit_plant_care_profile.dart
+++ src/app/lib/editplantcareprofile/edit_plant_care_profile.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:app/editplantcareprofile/edit_plant_care_profile_model.dart';
 
-//class EditPlantCareProfile extends StatefulWidget {
+class EditPlantCareProfile extends StatefulWidget {
+  final EditPlantCareProfileModel model;
 
-// }
+  const EditPlantCareProfile(this.model, {super.key});
+
+  @override
+  State<EditPlantCareProfile> createState() => _EditPlantCareProfileState();
+}
+
+class _EditPlantCareProfileState extends State<EditPlantCareProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(); // threw this here to stop error. TODO: replace with the widget
+  }
+}
--- src/app/lib/editplantcareprofile/edit_plant_care_profile_model.dart
+++ src/app/lib/editplantcareprofile/edit_plant_care_profile_model.dart
@@ -1,2 +1,15 @@
+import 'package:app/plantinstance/plant_info_model.dart';
 import 'package:app/utils/colour_scheme.dart';
 import 'package:flutter/material.dart';
+
+class EditPlantCareProfileModel extends ChangeNotifier {
+  int id;
+  LocationType location;
+  SoilType soilType;
+
+  int daysBetweenWatering;
+  int daysBetweenFertilising;
+  int daysBetweenRepotting;
+
+  EditPlantCareProfileModel.fromJSON(Map<String, dynamic> json)
+}
