51b95fc8ab699c97bd6845593115a677f6561d97
Callum Dixon <cdixon4335@gmail.com>
2022-09-20 11:27:11 +1000

    named constructors for model

--- src/app/lib/editplantcareprofile/edit_plant_care_profile_model.dart
+++ src/app/lib/editplantcareprofile/edit_plant_care_profile_model.dart
@@ -1,13 +1,21 @@
 import 'package:app/plantinstance/plant_info_model.dart';
-import 'package:app/utils/colour_scheme.dart';
 import 'package:flutter/material.dart';
 
-class EditPlantCareProfileModel extends ChangeNotifier {
+class EditPlantCareProfile extends ChangeNotifier {
   int? id;
   LocationType? location;
   SoilType? soilType;
-
   int? daysBetweenWatering;
   int? daysBetweenFertilising;
   int? daysBetweenRepotting;
+
+  EditPlantCareProfile.fromEmpty();
+  EditPlantCareProfile.fromProfile(PlantCareProfile profile) {
+    id = profile.id;
+    location = profile.location;
+    soilType = profile.soilType;
+    daysBetweenWatering = profile.daysBetweenWatering;
+    daysBetweenFertilising = profile.daysBetweenFertilising;
+    daysBetweenRepotting = profile.daysBetweenRepotting;
+  }
 }
