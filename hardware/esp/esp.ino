f4ed8ce358e6d5077559b0e867ada25bf06a9cbf
Satya Bhasale <95593545+venndiagram5@users.noreply.github.com>
2022-10-12 12:43:34 +1000

    small changes

--- hardware/esp/esp.ino
+++ hardware/esp/esp.ino
@@ -1,16 +1,20 @@
 #include "secrets.h"
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
 #define SERIAL_BAUD 115200 // make sure this is the same in arduino.ino
 const char *network = SSID;
 const char *password = PASS;
 String serialData = "";
+String baseUrl = "peclarke.pythonanywhere.com";
+String water = "/activity" String notification = "/send_plant_notification" bool previouslyDry = true
 
-void setup(void)
+	void
+	setup(void)
 {
 	Serial.begin(SERIAL_BAUD);
 	WiFi.begin(network, password);
-  Serial.print("...\n");
+	Serial.print("...\n");
 	while (WiFi.status() != WL_CONNECTED)
 	{
 		delay(500);
@@ -19,7 +23,7 @@ void setup(void)
 
 	for (int i = 0; i < 2; i++)
 	{
-    Serial.print("Connected!\n");
+		Serial.print("Connected!\n");
 		digitalWrite(LED_BUILTIN, HIGH);
 		delay(1000);
 		digitalWrite(LED_BUILTIN, LOW);
@@ -29,15 +33,23 @@ void setup(void)
 
 void loop(void)
 {
-  while (Serial.available())
-  {
-    char x = Serial.read();
-    if (x == '\r')
-      continue;
-    serialData += x;
-  }
-  int k = analogRead(0);
-  Serial.print("Received: ");
-   Serial.println(k);
-    delay(1000);
+	/* while (Serial.available()) {
+		char x = Serial.read();
+		if (x == '\r')
+		continue;
+		serialData += x;
+	} */
+	int rawMoistureData = analogRead(0);
+	int scaledMoistureData = map(result, 0, 400, 0, 100)
+								 Serial.print("Received: ");
+	Serial.println(k);
+	delay(1000);
+	if (previouslyDry && scaledMoistureData >= 50)
+	{
+		HTTPClient http;
+		previouslyDry = false;
+		http.begin(baseUrl + water);
+		http.addHeader("apiKey", APIKEY);
+		http.
+	}
 }
