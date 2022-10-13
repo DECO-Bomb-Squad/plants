857f729ac6abb754b0ef847a39a76016fd0c6c10
Satya Bhasale <95593545+venndiagram5@users.noreply.github.com>
2022-10-13 20:18:40 +1000

    hardware working!

--- hardware/arduino/arduino.ino
+++ hardware/arduino/arduino.ino
@@ -3,17 +3,19 @@ const int sensor_pin = 0;
 
 void setup()
 {
-	Serial.begin(SERIAL_BAUD); // same as ESP baud
+  Serial.begin(SERIAL_BAUD); // same as ESP baud
 }
 
 void loop()
 {
-	while (Serial.available())
+  while (Serial.available())
   {
-     int result = analogRead(sensor_pin);
-     result = map(result, 0, 400, 0, 100);
-     Serial.print("[");
-     Serial.print(result);
-     Serial.println("]");
+    // Read the moisture sensor
+    int result = analogRead(sensor_pin);
+    result = map(result, 0, 400, 0, 100);
+    // Send the results to be read by ESP Serial
+    Serial.print("[");
+    Serial.print(result);
+    Serial.println("]");
   }
 }
