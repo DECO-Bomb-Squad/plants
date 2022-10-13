3a5e5f93851c58c7520fee9dce7fb0fa63762524
Satya Bhasale <95593545+venndiagram5@users.noreply.github.com>
2022-10-13 17:08:31 +1000

    final for testing

--- hardware/arduino/arduino.ino
+++ hardware/arduino/arduino.ino
@@ -1,36 +1,19 @@
 #define SERIAL_BAUD 115200
-
-/*******************************************
-	In this code we have used "[]" to surround our command codes
-	As a bit of a proof of concept for how to use the XC4411 board
-*********************************************/
 const int sensor_pin = 0;
-
-String receivedCommand = "";
-bool dataIn = false;
-int result = 0;
-String serialData = "";
 
 void setup()
 {
-	// put your setup code here, to run once:
-
 	Serial.begin(SERIAL_BAUD); // same as ESP baud
 }
 
 void loop()
 {
 	while (Serial.available())
-	{ 
-    char x = Serial.read();
-    if (x == '\r')
-      continue;
-    serialData += x;
-		result = analogRead(sensor_pin);
-   result = map(result, 0, 400, 0, 100);
-   Serial.println(result);
-	}
- 
- 
-	delay(1000);
+  {
+     int result = analogRead(sensor_pin);
+     result = map(result, 0, 400, 0, 100);
+     Serial.print("[");
+     Serial.print(result);
+     Serial.println("]");
+  }
 }
--- hardware/esp/esp.ino
+++ hardware/esp/esp.ino
@@ -1,18 +1,25 @@
 #include "secrets.h"
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <Arduino_JSON.h>
 
 #define SERIAL_BAUD 115200 // make sure this is the same in arduino.ino
 const char *network = SSID;
-const char *password = PASS;
-String serialData = "";
-String baseUrl = "peclarke.pythonanywhere.com";
-String water = "/activity" String notification = "/send_plant_notification" bool previouslyDry = true
+const char *password = PASSWORD;
+bool previouslyDry = true;
+String receivedCommand = "";
+bool dataIn = false;
+int responseCode = 0;
+HTTPClient http;
+HTTPClient httpTime;
+HTTPClient httpWater;
+WiFiClient client;
 
-	void
-	setup(void)
+void setup(void)
 {
 	Serial.begin(SERIAL_BAUD);
+	// Cpnnect to the WiFi network
 	WiFi.begin(network, password);
 	Serial.print("...\n");
 	while (WiFi.status() != WL_CONNECTED)
@@ -21,35 +28,89 @@ String water = "/activity" String notification = "/send_plant_notification" bool
 		Serial.print("...\n");
 	}
 
-	for (int i = 0; i < 2; i++)
-	{
-		Serial.print("Connected!\n");
-		digitalWrite(LED_BUILTIN, HIGH);
-		delay(1000);
-		digitalWrite(LED_BUILTIN, LOW);
-		delay(1000);
-	}
+	Serial.print("Connected!\n");
 }
 
 void loop(void)
 {
-	/* while (Serial.available()) {
-		char x = Serial.read();
-		if (x == '\r')
-		continue;
-		serialData += x;
-	} */
-	int rawMoistureData = analogRead(0);
-	int scaledMoistureData = map(result, 0, 400, 0, 100)
-								 Serial.print("Received: ");
-	Serial.println(k);
-	delay(1000);
-	if (previouslyDry && scaledMoistureData >= 50)
-	{
-		HTTPClient http;
-		previouslyDry = false;
-		http.begin(baseUrl + water);
-		http.addHeader("apiKey", APIKEY);
-		http.
-	}
+  while (Serial.available())
+  {
+    char c = Serial.read();
+    if (c == '[')
+    {
+      //this is the start of the command string
+      receivedCommand = "";
+      dataIn = true;
+    }
+    //otherwise, we are still reading the command string:
+    else if (dataIn && c != ']')
+    {
+      receivedCommand += c;
+    }
+
+    else if (dataIn && c == ']')
+    { 
+      int moisture = receivedCommand.toInt();
+      Serial.print("Moisture: ");
+      Serial.println(moisture);
+      // The plant was dry but is now watered
+    	if (previouslyDry && moisture > 40)
+      {
+        // Send notification that watering has occured
+        http.begin(client, "http://peclarke.pythonanywhere.com/send_plant_notification");
+        http.addHeader("apiKey", APIKEY);
+        Serial.println("Attempting Notification API call");
+        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        responseCode = 0;
+        while (responseCode != 200) {
+          responseCode = http.POST("plantId=110&title=Watering%20detected&message=Your%20plant%20has%20just%20been%20watered!");
+        }
+        http.end();
+        
+        // Get the current datetime for logging watering activity
+        httpTime.begin(client, "http://worldtimeapi.org/api/timezone/Australia/Brisbane");
+        Serial.println("Attempting get DateTime API call");
+        responseCode = 0;
+        while (responseCode != 200) {
+          responseCode = httpTime.GET();
+        } 
+        // Format the time for activity API
+        JSONVar timeJson = JSON.parse(httpTime.getString());
+        String time = JSON.stringify(timeJson["datetime"]);
+        time.remove(time.length() - 1, 1);
+        time.remove(0, 1);
+        Serial.println(time);
+        String form = "plantId=110&time=" + time + "&activityTypeId=0";
+        Serial.println(form);
+        httpTime.end();
+
+        // Post the new watering activity
+        httpWater.begin(client, "http://peclarke.pythonanywhere.com/activity");
+        httpWater.addHeader("apiKey", APIKEY);
+        Serial.println("Attempting API call");
+        httpWater.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        responseCode = 0;
+        while (responseCode != 200) {
+          responseCode = httpWater.POST(form);
+        }
+        httpWater.end();
+        // Adjust current state to wet
+        previouslyDry = false;
+
+      // The plant was watered but is now dry and needs watering  
+      } else if (moisture <= 40 && !previouslyDry) {
+        http.begin(client, "http://peclarke.pythonanywhere.com/send_plant_notification");
+        http.addHeader("apiKey", APIKEY);
+        Serial.println("Attempting API call");
+        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        responseCode = 0;
+        while (responseCode != 200) {
+          responseCode = http.POST("plantId=110&title=Watering%20required&message=Your%20plant%20needs%20to%20be%20watered!");
+        }
+        http.end();
+        previouslyDry = true;
+      }
+      // The plant state has not changed to trigger a notification
+  }
+  }
 }
